9db39e61d2559fb47d78c7b509ec198cbb52e77c
Mark Angelo Sabado <markangelosabado10@gmail.com>
2024-12-17 10:22:25 +0800

    add debug

--- BT.API/Configurations/Database.cs
+++ BT.API/Configurations/Database.cs
@@ -11,17 +11,21 @@ namespace BT.API.Configurations
     {
         internal static void RegisterEntityFramework(WebApplicationBuilder builder)
         {
-            var a = builder.Configuration.GetConnectionString("DefaultSQL");
+            var connStr = builder.Configuration.GetConnectionString("DefaultSQL");
 
             builder.Services.AddDbContext<BTDbContext>(opts =>
             {
 
-                // var connStr = "Server=tcp:bt-server.database.windows.net,1433;Initial Catalog=BTDb;Persist Security Info=False;User ID=BtAdmin;Password=BtTracker!234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+                try
+                {
+                    opts.UseSqlServer(connStr, sqlServerOptions => sqlServerOptions.CommandTimeout(60));
 
-                // opts.UseSqlServer(connStr, sqlServerOptions => sqlServerOptions.CommandTimeout(60));
+                }
+                catch (Exception e)
+                {
 
-                opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQL"), sqlServerOptions => sqlServerOptions.CommandTimeout(60));
-                //opts.UseNpgsql(builder.Configuration.GetConnectionString("Default")!, sqlServerOptions => sqlServerOptions.CommandTimeout(60));
+                    throw new Exception($"{e.GetBaseException().Message} : ${connStr}");
+                }
             });
         }
 
